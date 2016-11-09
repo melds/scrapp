c6c597b5f86169d1ac9f37b70953a8c8ec0fb3c1
tweezy357 <ab.umparsalic@gmail.com>
2016-11-10 01:22:14 +0800

    added modify user page

--- resources/user_page.php
+++ resources/user_page.php
@@ -64,22 +64,62 @@
         <!-- /.container -->
     </nav>
         <div class="container">
-            <div class="row col-sm-6" id="avatar_main_div">
+            <div class="col-sm-6">
+            <div class="col-sm-12" id="avatar_main_div">
                 <div id="avatar_div" class="col-sm-6" align="center">
                     <i id="usericon" class="fa fa-user" aria-hidden="true"></i>
                     <button id="upload_profile" class="btn btn-sm">upload avatar</button>
                 </div>
                 <div class="col-sm-6" id="avatar_info">
+                    <h5 align="center"><b>PERSONAL INFORMATION</b></h5>
                     <p>Juan Dela Cruz</p>
                     <p>Tacloban City</p>
                     <p>Brgy 95, Marasbaras</p>
                     <p>juandelacruz@gmail.com</p>
                     <p>09469999999</p>
+                    <div align="center">
+                        <button class="btn btn-sm btn-danger"  data-toggle="modal" href='#modal-update'>UPDATE INFO</button>
+                    </div>
                 </div>
             </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="col-sm-12" id="history">
+                    <div class="col-sm-12">
+                        <h5><i class="fa fa-book" aria-hidden="true"></i> TRANSACTION HISTORY</h5>
+                        <div class="table-responsive"> 
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>TRANSACTION DATE</th>
+                                    <th>TYPE</th>
+                                    <th>JUNKSHOP</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>DEC 25,2016</td>
+                                    <td>BOTE</td>
+                                    <td>Scrapper Junk</td>
+                                </tr>
+                                <tr>
+                                    <td>DEC 25,2016</td>
+                                    <td>BOTE</td>
+                                    <td>Scrapper Junk</td>
+                                </tr>
+                                    <td>DEC 25,2016</td>
+                                    <td>BOTE</td>
+                                    <td>Scrapper Junk</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        </div>
+                    </div>
+                </div>
         </div>
         <div class="container">
-                                        <div class="modal fade" id="modal-login">
+            <div class="modal fade" id="modal-login">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-body">
@@ -121,6 +161,42 @@
             </div>
         </div>
         </div>
+
+        <div class="modal fade" id="modal-update">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <button id="btn_close_span" type="button" class="close" data-dismiss="modal">
+                  <span style="color:black">+</span>
+                </button>
+              <div class="modal-body">
+              <div class="container-fluid">
+                <div class="col-sm-10 col-sm-offset-1" id="modal_up_div">
+                <h4>UPDATE USER INFO</h4>
+                <form>
+                  <div class="form-group">
+                    <label for="name">NAME</label>
+                    <input type="text" class="form-control" id="name" placeholder="Enter Name">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">ADDRESS</label>
+                    <input type="text" class="form-control" id="address" placeholder="Address">
+                  </div>
+                  <div class="form-group">
+                    <label for="email">EMAIL</label>
+                    <input type="email" class="form-control" id="email" placeholder="Email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">PHONE</label>
+                    <input type="text" class="form-control" id="phone" placeholder="Phone">
+                  </div>
+                  <button id="but_sub" type="submit" class="btn btn-primary btn-block">Submit</button>
+                </form>
+                </div>
+                </div>
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
 
     <!-- Footer -->
     <footer>
