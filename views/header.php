66b91846adc885c340a4f002a675af160eac516f
melody01 <melodybadilla@gmail.com>
2016-11-10 12:08:24 +0800

    header udpate

--- views/header.php
+++ views/header.php
@@ -18,8 +18,9 @@
 	</head>
 	<body >
 	<!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+    <nav class="navbar navbar-default navbar topnav" role="navigation">
         <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -27,20 +28,29 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand topnav" href="#">ScrApp</a>
+                <a class="navbar-brand topnav" href="#">ScrApp  </a>
             </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#services">Services</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contact</a>
-                    </li>
+                   <?php if (Session::exists('user')): ?>
+                        
+                    <?php if (Session::get('user')['type']=='user'): ?>
+                          <li>
+                            <a href="<?=URL."post" ?>">Appoint Pick <i class="fa fa-calendar" aria-hidden="true"></i></a>
+                        </li> 
+                      <?php endif ?>  
+                        <li>
+                            <a href="<?=URL."logout" ?>">Logout <i class="fa fa-sign-out" aria-hidden="true"></i></a>
+                        </li>
+                    <?php else: ?>
+                        <li>
+                            <a href="<?=URL."login" ?>">Login <i class="fa fa-user" aria-hidden="true"></i></a>
+                        </li>
+                   <?php endif ?>
                 </ul>
             </div>
+            <!-- /.navbar-collapse -->
         </div>
+        <!-- /.container -->
     </nav>
